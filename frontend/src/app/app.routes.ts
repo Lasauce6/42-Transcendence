bec714a5da14511a2f929fbcd6e0b2d69e3ed87e
yamadou9 <yamadou9.dembele3@gmail.com>
2026-08-13 19:28:36 +0200

    add frontend structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('@features/home/home').then((m) => m.Home),
+  },
+
+  // {
+  //   path: 'chat',
+  //   loadComponent: () => import('@features/chat/chat').then(m => m.Chat),
+  // },
+  // {
+  //   path: 'admin',
+  //   loadChildren: () => import('@features/admin/admin.routes').then(m => m.ADMIN_ROUTES),
+  //   // canActivate: [roleGuard(['admin'])],   <-- ticket #48
+  // },
+
+  { path: '**', redirectTo: '' },
+];
