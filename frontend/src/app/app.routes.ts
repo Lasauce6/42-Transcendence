ad9fd5a114283ec1d8ecdde27c3eb7fc4eee0b7d
mukena_r <robinsonmukena@gmail.com>
2026-09-12 18:59:11 +0200

    finish

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
+import { OauthCallback } from './features/oauth-callback/oauth-callback';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,11 @@ export const routes: Routes = [
     title: 'Inscription',
   },
 
+  {
+    path: 'auth/callback/:provider',
+    component: OauthCallback,
+    title: 'Connexion en cours',
+  },
   {
     path: '',
     redirectTo: '/login',
