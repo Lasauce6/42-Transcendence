bc80ef2a6a706100f5176cee847dcb036bd9b088
mukena_r <robinsonmukena@gmail.com>
2026-09-15 16:46:08 +0200

    solve conflicts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { Header } from "./features/header/header";
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
